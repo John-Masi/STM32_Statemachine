cd4ef102c33cb6e41c53d8ddcba22c053de65954
John Masi <johnrmasi@gmail.com>
2026-08-31 23:21:03 -0500

    Update nvic.hpp

--- syscfg-nvic-rcc/include/nvic.hpp
+++ syscfg-nvic-rcc/include/nvic.hpp
@@ -11,12 +11,4 @@ struct NVIC_Typedef {
 
 #define NVIC ((NVIC_Typedef *)0xE000E100)
 
-void irq_init(void) {
-    SYSCFG->EXTICR4 |= (1 << 4);
-    EXTI->IMR |= (1 << 13);
-    EXTI->FTSR |= (1 << 13);
-
-    NVIC->ISER0[1] |= (1 << 8);
-}
-
 #endif 
