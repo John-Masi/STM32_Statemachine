5a3f173f4856ecbfa771016ade37666055e5b8d9
John Masi <johnrmasi@gmail.com>
2026-09-19 22:39:28 -0500

    Enable LCD updates based on LCD_ON definition
    Added conditional compilation for LCD display updates in state machine.

--- Src/statemachine.c
+++ Src/statemachine.c
@@ -13,6 +13,8 @@ static Statemachine int_machine = {
 
 Statemachine* const myMachine = &int_machine;
 
+#define LCD_ON 0
+
 void update_state(enum Event event,Statemachine* sm) {
 
 	if(event == EVENT_NULL) { return; }
@@ -27,8 +29,11 @@ void update_state(enum Event event,Statemachine* sm) {
 		case STATE_OFF:
 			if(event == BTN_PRESS) {
 				led_on();
-				clearLCD();
-				printStr("LED on");
+				#if LCD_ON
+					clearLCD();
+					printStr("LED on");
+				#endif
+				send_str("LED ON");
 				sm->current_state = STATE_ON;
 			}
 
@@ -37,8 +42,11 @@ void update_state(enum Event event,Statemachine* sm) {
 		case STATE_ON:
 			if(event == BTN_PRESS) {
 				led_off();
-				clearLCD();
-				printStr("LED off");
+				#if LCD_ON
+					clearLCD();
+					printStr("LED off");
+				#endif
+				send_str("LED OFF");
 				sm->current_state = STATE_OFF;
 			}
 	}
@@ -51,7 +59,6 @@ enum Event get_event(void) {
 	}
 
 	if(get_count()) {
-		led_on();
 		update_count(0);
 		ticks++;
 		if(ticks >= 20) {
